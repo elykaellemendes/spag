9c0df217a0b998f0402c41eab8c9dc13219cf0eb
Elykaelle Mendes <elykaellemendes@gmail.com>
2015-04-29 20:12:24 -0300

    Adicionado repositório de Pacientes.

--- src/main/java/br/org/apaebrasil/spag/dominio/repositorio/Pacientes.java
+++ src/main/java/br/org/apaebrasil/spag/dominio/repositorio/Pacientes.java
@@ -6,11 +6,15 @@
 package br.org.apaebrasil.spag.dominio.repositorio;
 
 import br.org.apaebrasil.spag.dominio.Paciente;
+import java.util.List;
 
 /**
  *
  * @author Pc
  */
 public interface Pacientes extends Repositorio<Paciente, Integer> {
+    public List<Paciente> porPaciente(String nome);
+    public List<Paciente> porCidade(String cidade);
+    public List<Paciente> porCartaoSus(String cartaoSus);
     
 }
--- src/main/java/br/org/apaebrasil/spag/infraestrutura/repositorio/PacientesHibernate.java
+++ src/main/java/br/org/apaebrasil/spag/infraestrutura/repositorio/PacientesHibernate.java
@@ -7,11 +7,27 @@ package br.org.apaebrasil.spag.infraestrutura.repositorio;
 
 import br.org.apaebrasil.spag.dominio.Paciente;
 import br.org.apaebrasil.spag.dominio.repositorio.Pacientes;
+import java.util.List;
 
 /**
  *
  * @author Pc
  */
 public class PacientesHibernate extends RepositorioHibernate<Paciente, Integer> implements Pacientes {
+
+    @Override
+    public List<Paciente> porPaciente(String nome) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Paciente> porCidade(String nome) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Paciente> porCartaoSus(String cartaoSus) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
   
 }
