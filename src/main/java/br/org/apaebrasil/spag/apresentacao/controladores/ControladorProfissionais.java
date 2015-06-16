c15c8dd39935340d1c8bc338a95907710a5ec9b0
Elykaelle Mendes <elykaellemendes@gmail.com>
2015-06-16 20:47:59 -0300

    Removidos println desnecessários.

--- src/main/java/br/org/apaebrasil/spag/apresentacao/controladores/ControladorProfissionais.java
+++ src/main/java/br/org/apaebrasil/spag/apresentacao/controladores/ControladorProfissionais.java
@@ -20,39 +20,28 @@ import javax.inject.Named;
  */
 @Named
 @ViewScoped
-public class ControladorProfissionais implements Serializable{
-    
+public class ControladorProfissionais implements Serializable {
+
     private static final long serialVersionUID = 1;
-    
+
     @Inject
     private Profissionais profissionais;
-    
-    private Profissional profissional = new Profissional();
-    
-    private List<Profissional> todosProfissionais;
-    
-    public void consultar() {
-        System.out.println("Consultando...");
-        todosProfissionais = profissionais.recuperarTodos();
 
-        for (Profissional pf : todosProfissionais) {
-            System.out.println(pf);
-        }
+    private Profissional profissional = new Profissional();
+
+    private List<Profissional> todosProfissionais;
+
+    public void consultar() {
+        todosProfissionais = profissionais.recuperarTodos();
     }
 
     public void novo() {
-        System.out.println("Novo...");
         profissional = new Profissional();
     }
 
     public void adicionar() {
-        System.out.println("Adicionando...");
         profissionais.inserir(profissional);
-        System.out.println("Adicionado...");
-
-        System.out.println("Consultando no adicionar...");
         consultar();
-        System.out.println("Consultado no adicionar...");
     }
 
     public Profissional getProfissional() {
@@ -66,5 +55,4 @@ public class ControladorProfissionais implements Serializable{
     public Especializacao[] getEspecializacao() {
         return Especializacao.values();
     }
-    
 }
