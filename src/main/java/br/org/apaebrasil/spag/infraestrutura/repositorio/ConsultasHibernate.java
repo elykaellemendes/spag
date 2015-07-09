15a84d2b516b052701833c00bcfb94e4885f4bb4
Fabricio Cabral <fabriciofx@gmail.com>
2015-07-09 18:38:57 -0300

    Refatoração da busca de consultas por paciente.
    Entretanto, note que esta implementação possui
    uma "gambiarra", no caso, a concatenação do
    código do paciente com a consulta SQL. Isto foi
    feito devido a um problema de diferença de tipos
    do Hibernate. O atributo código é do tipo int e,
    acredito, que o tipo que está no BD é do tipo
    long. Mas para ter certeza mesmo se esta é a causa
    do problema, devemos trocar o tipo de código por
    long.

--- src/main/java/br/org/apaebrasil/spag/infraestrutura/repositorio/ConsultasHibernate.java
+++ src/main/java/br/org/apaebrasil/spag/infraestrutura/repositorio/ConsultasHibernate.java
@@ -9,16 +9,27 @@ import java.util.List;
 import br.org.apaebrasil.spag.dominio.Consulta;
 import br.org.apaebrasil.spag.dominio.Paciente;
 import br.org.apaebrasil.spag.dominio.repositorio.Consultas;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Query;
 
 /**
  *
  * @author Pc
  */
 public class ConsultasHibernate extends RepositorioHibernate<Consulta, Integer> implements Consultas {
+
     @Override
-    public List<Consulta> porPaciente(Paciente nome) {
-        return manager.createQuery("from Consulta c where c.nome = :nome").getResultList();
+    public List<Consulta> porPaciente(Paciente paciente) {
+        Objects.requireNonNull(paciente, "paciente inválido");
+
+        // Isto aqui é uma GAMBIARRA!!!
+        // Por alguma razão o hibernate está acusando que os tipos são diferentes
+        // e por isso não deixa utilizar o query.setParameter()
+        Query query = manager.createQuery("FROM Consulta c WHERE c.paciente = " + paciente.getCodigo());
+
+        return query.getResultList();
     }
 
     @Override
