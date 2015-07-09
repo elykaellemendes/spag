91fe972e5e5397c7be51867a389ffe03712c9e41
Fabricio Cabral <fabriciofx@gmail.com>
2015-07-09 17:12:38 -0300

    Adicionado teste para o JPA via CDI.

---
+++ src/main/java/br/org/apaebrasil/spag/teste/TesteSPAGPU.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.apaebrasil.spag.teste;
+
+//import br.org.apaebrasil.spag.dominio.Paciente;
+//import br.org.apaebrasil.spag.dominio.repositorio.Pacientes;
+//import br.org.apaebrasil.spag.infraestrutura.repositorio.PacientesHibernate;
+//import br.org.apaebrasil.spag.infraestrutura.repositorio.util.EntityManagerProducer;
+//import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author fbc
+ */
+public class TesteSPAGPU {
+    public static void main(String[] args) {
+//        EntityManagerProducer producer = new EntityManagerProducer();
+//        EntityManager manager = producer.createEntityManager();
+//        
+//        Pacientes pacientes = new PacientesHibernate(manager);
+//        
+//        for (Paciente p : pacientes.recuperarTodos()) {
+//            System.out.println(p);
+//        }
+//        
+//        System.out.println("PACIENTE: " + pacientes.recuperar(4));
+//        System.out.println("PACIENTE: " + pacientes.recuperar(3));
+    }
+}
