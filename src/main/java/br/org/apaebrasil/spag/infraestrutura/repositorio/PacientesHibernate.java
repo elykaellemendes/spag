728a103419806da95aa04f8156bc41cebf7d1a84
Fabricio Cabral <fabriciofx@gmail.com>
2015-07-09 18:42:42 -0300

    Refatoração geral das buscas dos pacientes.

--- src/main/java/br/org/apaebrasil/spag/infraestrutura/repositorio/PacientesHibernate.java
+++ src/main/java/br/org/apaebrasil/spag/infraestrutura/repositorio/PacientesHibernate.java
@@ -8,26 +8,41 @@ package br.org.apaebrasil.spag.infraestrutura.repositorio;
 import br.org.apaebrasil.spag.dominio.Paciente;
 import br.org.apaebrasil.spag.dominio.repositorio.Pacientes;
 import java.util.List;
+import java.util.Objects;
+import javax.persistence.Query;
 
 /**
  *
  * @author Pc
  */
 public class PacientesHibernate extends RepositorioHibernate<Paciente, Integer> implements Pacientes {
-
     @Override
     public List<Paciente> porNome(String nome) {
-        return manager.createQuery("from Paciente p where p.nome = :nome").getResultList();
+        Objects.requireNonNull(nome, "nome inválido");
+
+        Query query = manager.createQuery("FROM Paciente p WHERE p.nome LIKE :nome");
+        query.setParameter("nome", "%" + nome + "%");
+
+        return query.getResultList();
     }
 
     @Override
     public List<Paciente> porCidade(String cidade) {
-        return manager.createQuery("from Paciente p where p.cidade = :cidade").getResultList();
+        Objects.requireNonNull(cidade, "cidade inválida");
+
+        Query query = manager.createQuery("FROM Paciente p WHERE p.endereco.cidade = :cidade");
+        query.setParameter("cidade", cidade);
+
+        return query.getResultList();
     }
 
     @Override
     public List<Paciente> porCartaoSus(String cartaoSus) {
-        return manager.createQuery("from Paciente p where p.cartaosus = :cartaosus").getResultList();
-    }
+        Objects.requireNonNull(cartaoSus, "cartão do SUS inválido");
 
+        Query query = manager.createQuery("FROM Paciente p WHERE p.cartaoSus = :cartaoSus");
+        query.setParameter("cartaoSus", cartaoSus);
+        
+        return query.getResultList();
+    }
 }
