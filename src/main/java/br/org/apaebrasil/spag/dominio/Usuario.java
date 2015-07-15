8ae8429e374d79a9577f1b1ddd6211707e20f616
Fabricio Cabral <fabriciofx@gmail.com>
2015-07-14 21:02:25 -0300

    Adicionado código para criptografar a senha do ousuário.

--- src/main/java/br/org/apaebrasil/spag/dominio/Usuario.java
+++ src/main/java/br/org/apaebrasil/spag/dominio/Usuario.java
@@ -6,6 +6,8 @@
 package br.org.apaebrasil.spag.dominio;
 
 import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import javax.persistence.Embeddable;
 
 /**
@@ -17,12 +19,33 @@ public class Usuario implements Serializable {
     private String login;
     private String senha;
 
+    private static char[] hexCodes(byte[] text) {
+        char[] hexOutput = new char[text.length * 2];
+        String hexString;
+
+        for (int i = 0; i < text.length; i++) {
+            hexString = "00" + Integer.toHexString(text[i]);
+            hexString.toUpperCase().getChars(hexString.length() - 2,
+                    hexString.length(), hexOutput, i * 2);
+        }
+        return hexOutput;
+    }
+
+    private String criptografar(String senha) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return new String(hexCodes(md.digest(senha.getBytes())));
+        } catch (NoSuchAlgorithmException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
     public Usuario() {
     }
 
-    public Usuario(String login,String senha) {
+    public Usuario(String login, String senha) {
         this.login = login;
-        this.senha = senha;
+        this.senha = criptografar(senha);
     }
 
     public String getLogin() {
